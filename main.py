058464c06653380a3dcdebbfca25ee79840f114b
Ikenna Enyidede <stephencharles02@gmail.com>
2023-10-28 21:45:50 +0100

    backend test

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from flask import Flask, request, jsonify
+"""
+Hostober Flask App
+"""
+
+import os
 from flask_cors import CORS
 import requests
-import os
 from dotenv import load_dotenv
 
 # Load environment variables from .env file during development
@@ -15,8 +18,10 @@ CF_API_KEY = os.getenv('CF_API_KEY')
 CF_API_EMAIL = os.getenv('CF_API_EMAIL')
 API_URL = f'https://api.cloudflare.com/client/v4/zones/{ZONE_ID}/dns_records'
 
+
 @app.route('/', methods=['GET'])
 def proxy():
+    ''' This is a docstring for this function'''
     headers = {
         'X-Auth-Email': CF_API_EMAIL,
         'X-Auth-Key': CF_API_KEY,
